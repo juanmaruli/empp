4514c57cf1c3ca197a42eed500f3f9dc410f3e21
juanmaruli <juanmaruli17@gmail.com>
2022-07-31 20:38:15 +0700

    Modifikasi class

--- gitpush/src/DepartmentApp/hrd.java
+++ gitpush/src/DepartmentApp/hrd.java
@@ -12,5 +12,23 @@ package DepartmentApp;
 public class hrd {
     int id;
     String dep_name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDep_name() {
+        return dep_name;
+    }
+
+    public void setDep_name(String dep_name) {
+        this.dep_name = dep_name;
+    }
+    
+    
     
 }
--- gitpush/src/DepartmentApp/manager.java
+++ gitpush/src/DepartmentApp/manager.java
@@ -10,5 +10,25 @@ package DepartmentApp;
  * @author juanm
  */
 public class manager {
+    int id;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
     
 }
